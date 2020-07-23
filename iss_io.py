63ea654e7c342ae5cef634e8bfde883b219af283
MasterRoshan <t22thejohnson@gmail.com>
2020-07-22 23:09:35 -0400

    completed pass challenge
    switched from the time lib to datetime to consolidate
    
    switched the wording on the first challenge

--- iss_io.py
+++ iss_io.py
@@ -1,7 +1,7 @@
 import requests
-import time
+from datetime import datetime, timedelta
 import argparse
-
+from urllib.parse import urlencode
 API_URL = 'http://api.open-notify.org/'
 
 parser = argparse.ArgumentParser(
@@ -31,13 +31,26 @@ if args.command == 'loc':
 
     location = resp_json.get('iss_position')
 
-    current_time = time.strftime('%H:%M:%S', time.localtime())
+    current_time = datetime.strftime(datetime.now(), '%H:%M:%S')
 
     print(
-        f"\nThe current location of the ISS at {current_time}\n"
-        f"latitute: {location.get('latitude')}\n"
-        f"longitude: {location.get('longitude')}\n"
+        f'\nThe current location of the ISS at {current_time}'
+        f" is ({location.get('latitude')}, {location.get('longitude')})\n"
     )
 
 elif args.command == 'pass':
-    pass
+    query = {
+        'lat': args.lat,
+        'lon': args.lon
+    }
+    url_query = urlencode(query)
+    resp_json = requests.get(f'{API_URL}iss-pass.json?{url_query}').json()
+
+    timestamp = resp_json.get('response')[0].get('risetime')
+    next_pass = datetime.fromtimestamp(timestamp)
+    next_pass_time = datetime.strftime(next_pass, '%H:%M:%S')
+    duration = timedelta(seconds=resp_json.get('response')[0].get('duration'))
+
+    print(f'\nThe ISS will be overhead ({args.lat},{args.lon}) '
+          f'at {next_pass_time} for {str(duration)}\n'
+          )
