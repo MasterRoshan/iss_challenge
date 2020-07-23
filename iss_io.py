68f3aea0c710b0bef8e224d9b1564967bd568afb
MasterRoshan <t22thejohnson@gmail.com>
2020-07-23 10:15:26 -0400

    factored into functions and added docstrings

--- iss_io.py
+++ iss_io.py
@@ -2,31 +2,18 @@ import requests
 from datetime import datetime, timedelta
 import argparse
 from urllib.parse import urlencode
+
 API_URL = 'http://api.open-notify.org/'
 
-parser = argparse.ArgumentParser(
-    description='Get some information about the ISS over REST'
-)
-subparsers = parser.add_subparsers(help='commands', dest='command')
-loc_parser = subparsers.add_parser(
-    'loc', help='get the current location of the ISS'
-)
-pass_parser = subparsers.add_parser(
-    'pass', help='pass coordinates with --lat and --lon'
-)
-pass_parser.add_argument('--lat', required=True,
-                         type=float, help='latitude coordinate'
-                         )
-pass_parser.add_argument('--lon', required=True, type=float,
-                         help='longitude coordinate'
-                         )
-pass_parser = subparsers.add_parser(
-    'people', help='get the current astronauts aboard the ISS'
-)
 
-args = parser.parse_args()
+def print_current_location():
+    """Prints out the current coordinates of the ISS
 
-if args.command == 'loc':
+    Returns
+    -------
+    None
+
+    """
     resp_json = requests.get(f'{API_URL}iss-now.json').json()
 
     location = resp_json.get('iss_position')
@@ -38,10 +25,25 @@ if args.command == 'loc':
         f" is ({location.get('latitude')}, {location.get('longitude')})\n"
     )
 
-elif args.command == 'pass':
+
+def print_next_pass(lat: float, lon: float):
+    """Prints when the next time the ISS will be overhead for given coordinates
+
+    Parameters
+    ----------
+    lat : float
+        Description of parameter `lat`.
+    lon : float
+        Description of parameter `lon`.
+
+    Returns
+    -------
+    None
+
+    """
     query = {
-        'lat': args.lat,
-        'lon': args.lon
+        'lat': lat,
+        'lon': lon
     }
     url_query = urlencode(query)
     resp_json = requests.get(f'{API_URL}iss-pass.json?{url_query}').json()
@@ -51,11 +53,60 @@ elif args.command == 'pass':
     next_pass_time = datetime.strftime(next_pass, '%H:%M:%S')
     duration = timedelta(seconds=resp_json.get('response')[0].get('duration'))
 
-    print(f'\nThe ISS will be overhead ({args.lat},{args.lon}) '
+    print(f'\nThe ISS will be overhead ({lat},{lon}) '
           f'at {next_pass_time} for {str(duration)}\n'
           )
 
-elif args.command == 'people':
+
+def print_people():
+    """Prints the names of the astronauts aboard the ISS
+
+    Returns
+    -------
+    None
+
+    """
     resp_json = requests.get(f'{API_URL}astros.json').json()
     names = [x.get('name') for x in resp_json.get('people')]
     print(f'\nAstronauts aboard the ISS: {", ".join(names)}\n')
+
+
+def main():
+    """Parses command line arguments and calls appropriate print function
+
+    Returns
+    -------
+    None
+
+    """
+    parser = argparse.ArgumentParser(
+        description='Get some information about the ISS over REST'
+    )
+    subparsers = parser.add_subparsers(help='commands', dest='command')
+    loc_parser = subparsers.add_parser(
+        'loc', help='get the current location of the ISS'
+    )
+    pass_parser = subparsers.add_parser(
+        'pass', help='pass coordinates with --lat and --lon'
+    )
+    pass_parser.add_argument('--lat', required=True,
+                             type=float, help='latitude coordinate'
+                             )
+    pass_parser.add_argument('--lon', required=True, type=float,
+                             help='longitude coordinate'
+                             )
+    pass_parser = subparsers.add_parser(
+        'people', help='get the current astronauts aboard the ISS'
+    )
+    args = parser.parse_args()
+
+    if args.command == 'loc':
+        print_current_location()
+    elif args.command == 'pass':
+        print_next_pass(args.lat, args.lon)
+    elif args.command == 'people':
+        print_people()
+
+
+if __name__ == "__main__":
+    main()
